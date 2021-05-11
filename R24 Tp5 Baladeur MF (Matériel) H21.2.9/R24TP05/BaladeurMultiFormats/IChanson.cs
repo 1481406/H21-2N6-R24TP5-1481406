cd208d32d7a5503ce17a27fd7f8abcb87c77d921
steven <1481406@cegepmontpetit.ca>
2021-05-10 21:49:00 -0700

    added interface Ichanson.

---
+++ R24 Tp5 Baladeur MF (Matériel) H21.2.9/R24TP05/BaladeurMultiFormats/IChanson.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BaladeurMultiFormats
+{
+    interface IChanson
+    {        
+         int Annee { get; }
+        string Artiste { get; }
+        string Format { get; }
+        string NomFichier { get; }
+        string Paroles { get; }
+        string Titre { get; }
+
+
+
+
+
+         void Ecrire(string pParoles);
+        void EcrireEntete(StreamWriter pobjFichier);
+
+        void EcrireParoles(StreamWriter pobjFichier, string pParoles);
+
+        void LireEntete();
+
+        void LireParoles(StreamWriter pobjfichier, string pParoles);
+
+        void SauterEntete(StreamReader pobjFichier);
+
+
+
+    }
+}
