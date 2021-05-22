0c65761d3a0f7e94c9b5067f2b27a576d7e01472
steven <1481406@cegepmontpetit.ca>
2021-05-21 20:20:37 -0700

    Classe Baladeur Défini

---
+++ R24 Tp5 Baladeur MF (Matériel) H21.2.9/R24TP05/BaladeurMultiFormats/Baladeur.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BaladeurMultiFormats
+{
+    public class Baladeur : IBaladeur
+    {
+
+        private const string NOM_RÉPERTOIRE = "Chansons";
+        private List<Chanson> m_colChansons;
+
+
+
+        public int NbChansons
+        {
+            get;
+        }
+
+        public void AfficherLesChansons(ListView pListView)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Chanson ChansonAt(int pIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConstruireLaListeDesChansons()
+        {
+            foreach (var Chanson in m_colChansons)
+            {
+                if ()
+                {
+
+                }
+            }
+        }
+
+        public void ConvertirVersAAC(int pIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConvertirVersMP3(int pIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConvertirVersWMA(int pIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Baladeur()
+        {
+            m_colChansons.Clear();
+        }
+
+
+    }
+}
